fff136aa92b527439e601885f4ef6b63ca529da2
SUWAATHMI RAVINDRAN <122599811+Suwaathmi@users.noreply.github.com>
2026-02-13 04:47:45 +0530

    Update variables.tf with AWS configuration variables for EC2

--- variables.tf
+++ variables.tf
@@ -1,1 +1,23 @@
-# No variables needed for local Docker setup
+# AWS Configuration Variables
+
+variable "ec2_key_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "environment" {
+  description = "Environment for the deployment (e.g., dev, staging, production)"
+  type        = string
+}
+
+variable "cidr_blocks" {
+  description = "CIDR blocks for the VPC"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
