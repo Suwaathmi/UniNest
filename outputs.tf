932d686d33bacbb5d969c9d82c6488e8ee9af89f
SUWAATHMI RAVINDRAN <122599811+Suwaathmi@users.noreply.github.com>
2026-02-13 04:37:43 +0530

    Update outputs.tf to output EC2 instance details

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
-output "frontend_container_id" {
-  value = docker_container.frontend.id
+output "instance_public_ip" {
+  value = aws_instance.my_instance.public_ip
 }
 
-output "backend_container_id" {
-  value = docker_container.backend.id
+output "instance_id" {
+  value = aws_instance.my_instance.id
+}
+
+output "security_group_id" {
+  value = aws_instance.my_instance.vpc_security_group_ids[0]
+}
+
+output "ssh_command" {
+  value = "ssh -i my-key.pem ec2-user@${aws_instance.my_instance.public_ip}"
 }
