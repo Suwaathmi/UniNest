25ef3be75ce34adf31643415188c1b6fc913a9a3
SUWAATHMI RAVINDRAN <122599811+Suwaathmi@users.noreply.github.com>
2026-02-20 01:11:18 +0530

    Fix security group outputs to use tolist()
    Updated security group outputs to use tolist() for compatibility.

--- outputs.tf
+++ outputs.tf
@@ -16,13 +16,13 @@ output "jenkins_server_id" {
   value = aws_instance.jenkins_server.id
 }
 
-# Security Groups
+# Security Groups (fix: use tolist() or output full set)
 output "app_server_sg" {
-  value = aws_instance.app_server.vpc_security_group_ids[0]
+  value = tolist(aws_instance.app_server.vpc_security_group_ids)[0]
 }
 
 output "jenkins_server_sg" {
-  value = aws_instance.jenkins_server.vpc_security_group_ids[0]
+  value = tolist(aws_instance.jenkins_server.vpc_security_group_ids)[0]
 }
 
 # SSH Commands (using your uninest-key.pem file)
