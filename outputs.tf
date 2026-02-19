ea75446940cd10a88e4a2ff55dde757375db84e3
SUWAATHMI RAVINDRAN <122599811+Suwaathmi@users.noreply.github.com>
2026-02-20 00:58:53 +0530

    Add outputs for app server and Jenkins server
    Added outputs for app server and Jenkins server IPs, IDs, security groups, and SSH commands.

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,35 @@
-output "instance_public_ip" {
-  value = aws_instance.my_instance.public_ip
+# Public IPs
+output "app_server_ip" {
+  value = aws_instance.app_server.public_ip
 }
 
-output "instance_id" {
-  value = aws_instance.my_instance.id
+output "jenkins_server_ip" {
+  value = aws_instance.jenkins_server.public_ip
 }
 
-output "security_group_id" {
-  value = aws_instance.my_instance.vpc_security_group_ids[0]
+# Instance IDs
+output "app_server_id" {
+  value = aws_instance.app_server.id
 }
 
-output "ssh_command" {
-  value = "ssh -i my-key.pem ec2-user@${aws_instance.my_instance.public_ip}"
+output "jenkins_server_id" {
+  value = aws_instance.jenkins_server.id
+}
+
+# Security Groups
+output "app_server_sg" {
+  value = aws_instance.app_server.vpc_security_group_ids[0]
+}
+
+output "jenkins_server_sg" {
+  value = aws_instance.jenkins_server.vpc_security_group_ids[0]
+}
+
+# SSH Commands (using your uninest-key.pem file)
+output "app_server_ssh" {
+  value = "ssh -i uninest-key.pem ubuntu@${aws_instance.app_server.public_ip}"
+}
+
+output "jenkins_server_ssh" {
+  value = "ssh -i uninest-key.pem ubuntu@${aws_instance.jenkins_server.public_ip}"
 }
